a6bcbfe797f0fcaa41d8720fb147919db7518522
Amit Thapa <amitt@unicourt.com>
2018-03-15 19:09:38 +0530

    Temp App

---
+++ website/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My Shop</title>
+	</head>
+	<body>
+		<?php 
+			$json = file_get_contents('http://product-service');
+			echo "<h3>Products</h3>";
+			$obj = json_decode($json);
+			
+			$products = $obj->products;
+			foreach ($products as $product) {
+				echo "<li>$product</li>";
+			}
+		 ?>
+	</body>
+</html>
